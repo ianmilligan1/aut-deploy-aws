aaf6cf416f9fe8d055d6c60ff0600de93d6cadaf
ianmilligan1 <i2milligan@uwaterloo.ca>
2017-12-17 12:01:21 -0500

    adding unmount note

--- loader.sh
+++ loader.sh
@@ -6,3 +6,7 @@ chown -R ubuntu:ubuntu /data
 mdkir /data/labour
 
 # then transfer data into /data/labour using WASAPI or other call
+
+umount -d /dev/xvdf
+
+# we then need to detach using AMAZON CLI, not quite sure how to do that yet in this workflow
